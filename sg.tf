d089847d870736e5b1e26fd2684be9cfab4a0232
Vaidhiyanathan R <vaidhi.r03@gmail.com>
2024-12-06 21:06:39 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "eks_sg" {
+  name        = "eks-sg"
+  description = "Allow all traffic for EKS"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-sg"
+  }
+}
